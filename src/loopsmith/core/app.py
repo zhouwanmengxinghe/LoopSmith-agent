2a29d3bd7ec623e546523428be1c500468d37d8f
zhouwanmengxinghe <129480419+zhouwanmengxinghe@users.noreply.github.com>
2026-09-05 23:22:55 +0800

    changed:   .env

--- src/loopsmith/core/app.py
+++ src/loopsmith/core/app.py
@@ -275,8 +275,12 @@ class CoreApp:
 
         loop = asyncio.get_running_loop()
         shutdown = asyncio.Event()
-        loop.add_signal_handler(signal.SIGINT, shutdown.set)
-        loop.add_signal_handler(signal.SIGTERM, shutdown.set)
+        try:
+            loop.add_signal_handler(signal.SIGINT, shutdown.set)
+            loop.add_signal_handler(signal.SIGTERM, shutdown.set)
+        except NotImplementedError:
+    # Windows不支持 add_signal_handler，跳过异步信号注册
+            pass
 
         await shutdown.wait()
 
